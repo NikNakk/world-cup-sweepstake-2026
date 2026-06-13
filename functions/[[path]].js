f242e6e0d671277f4ece6468da36e095fdf7d196
Nick Kennedy <nick@nick-kennedy.com>
2026-06-13 20:51:14 +0100

    Shorten Pages HTML cache to 30 seconds

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -11,7 +11,7 @@ const SECURITY_HEADERS = {
 
 const HTML_HEADERS = {
   'content-type': 'text/html; charset=utf-8',
-  'cache-control': 'no-cache, must-revalidate',
+  'cache-control': 'public, max-age=30',
   ...SECURITY_HEADERS,
 };
 
